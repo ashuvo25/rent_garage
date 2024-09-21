ed46e2f1b0ce2183ab254ecdc7f251442c7c2b03
ashuvo25 <iqbalmdshuvo@outlook.com>
2024-09-21 09:10:16 +0600

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/example/rent_garadge/RentGaradge.java
+++ src/main/java/com/example/rent_garadge/RentGaradge.java
@@ -5,17 +5,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class RentGaradge extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+//        stage.getIcons().add(new Image(getClass().getResourceAsStream("/com/example/rent_garadge/logo.png")));
+//
+//        stage.initStyle(StageStyle.UNDECORATED);
+
         FXMLLoader fxmlLoader = new FXMLLoader(RentGaradge.class.getResource("prev_imgs.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 360, 500);
 
 
-        stage.setTitle("RENT GARADGE!");
+        stage.setTitle("RENT GARAGE!");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/rent_garadge/webController.java
@@ -0,0 +1,4 @@
+package com.example.rent_garadge;
+
+public class webController {
+}
